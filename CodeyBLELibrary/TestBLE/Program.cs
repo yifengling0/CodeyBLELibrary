ffd81b73d31e46f6be1a697f83948f7e891aaea5
liufeng <liu.feng@furuno-dl.com>
2018-03-26 21:15:01 +0800

    提交整个项目初始代码

---
+++ CodeyBLELibrary/TestBLE/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BluetoothLE;
+
+namespace TestBLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GattSampleContext context  = GattSampleContext.Context;
+
+            context.StartEnumeration();
+
+            while (Console.ReadKey().KeyChar !='Q')
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
